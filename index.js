8bcb4b96467bff9905c1e3df380277dcc8030b1c
Ilya <ilya.shuvatov1@gmail.com>
2022-11-09 07:18:08 +0100

    Task4

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 let userName = prompt('Как вас зовут?')
 userName = userName.toLowerCase().trim()
 alert(`Вас зовут ${userName}`)
+
+// Задание #4
+
+let userAge = prompt('Сколько вам лет?')
+userAge = +userAge.trim()
+alert(`Вас зовут ${userName} и вам ${userAge} лет`)
